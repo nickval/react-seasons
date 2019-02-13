dc0a4fae06612fc2553eee562ba60b854346867c
nikolay.ignatyev <nikolay.ignatyev@progforce.com>
2019-02-13 21:15:37 +0200

    init commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+    return(
+        <div>Season is...</div>
+    )
+}
+
+export default SeasonDisplay;
